9c2b961e52870e61b051545c8ad3910c4e308740
Erin-Rooney <61806923+Erin-Rooney@users.noreply.github.com>
2020-08-11 14:54:48 -0700

    Changes to GHG code

--- code/ER-13-GHGftc.R
+++ code/ER-13-GHGftc.R
@@ -10,21 +10,36 @@ toolik = ghg_csv$Site=="TOOL"
 healy = ghg_csv$Site=="HEALY"
 control = ghg_csv$TRT=="CON"
 ftc = ghg_csv$TRT=="FTC"
-Day
-Day_1 = ghg_csv$Day=="1"
-Day_4 = ghg_csv$Day=="4"
-Day_7 = ghg_csv$Day=="7"
-Day_14 = ghg_csv$Day=="14"
+DAY
+Day_1 = ghg_csv$DAY=="1"
+Day_4 = ghg_csv$DAY=="4"
+Day_7 = ghg_csv$DAY=="7"
+Day_14 = ghg_csv$DAY=="14"
 
 library(ggplot2)
 
 
-ggplot(ghg_csv, aes(x=TRT, y=ug_13C_g_soil, fill=DAY)) + geom_boxplot()
+ggplot(ghg_csv, aes(x=TRT, y=gain_ug_per_gOC, fill=DAY)) + geom_boxplot()
 
 
-ggplot(ghg_csv, aes(x=Site, y=ug_13C_g_soil, fill=DAY)) + geom_boxplot()
+ggplot(ghg_csv, aes(x=Site, y=gain_ug_per_gOC, fill=DAY)) + geom_boxplot()
 
 ggplot(ghg_csv, aes(x=Site, y=ug_13C_g_soil, fill=TRT)) + geom_boxplot()
+
+################
+
+ghg_aov1 = aov(data = ghg_csv, gain_ug_per_gOC ~ Site)
+summary(ghg_aov1)
+
+
+ghg_aov2 = aov(data = ghg_csv, gain_ug_per_gOC ~ DAY)
+summary(ghg_aov2)
+
+
+ghg_aov3 = aov(data = ghg_csv, gain_ug_per_gOC ~ Site*DAY)
+summary(ghg_aov3)
+
+###############
 
 
 ggplot(ghg_csv, aes(x=TRT, y=ug_12C_g_soil, fill=DAY)) + geom_boxplot()
