6950342d2fd284b09dc33ed0806846feb48300a3
Erin-Rooney <61806923+Erin-Rooney@users.noreply.github.com>
2020-10-06 11:29:32 -0700

    ggplot edits/ formatting modifications

--- code/ER-13-GHGftc.R
+++ code/ER-13-GHGftc.R
@@ -72,6 +72,28 @@ ftc_dat = ftc_dat %>%
       )
   }
 
+theme_er2 <- function() {  # this for all the elements common across plots
+  theme_bw() %+replace%
+    theme(legend.position = "top",
+          legend.key=element_blank(),
+          legend.title = element_blank(),
+          legend.text = element_text(size = 12),
+          legend.key.size = unit(1.5, 'lines'),
+          panel.border = element_rect(color="black",size=2, fill = NA),
+          plot.title = element_text(hjust = 0.5, size = 14),
+          plot.subtitle = element_text(hjust = 0.5, size = 12, lineheight = 1.5),
+          axis.text = element_text(size = 12, color = "black"),
+          axis.title = element_text(size = 12, face = "bold", color = "black"),
+          # formatting for facets
+          panel.background = element_blank(),
+          strip.background = element_rect(colour="black", fill="black"), #facet formatting
+          panel.spacing.x = unit(1.5, "lines"), #facet spacing for x axis
+          panel.spacing.y = unit(1.5, "lines"), #facet spacing for x axis
+          strip.text.x = element_text(size=12, face="bold"), #facet labels
+          strip.text.y = element_text(size=12, face="bold", angle = 270) #facet labels
+    )
+}
+
 library(ggplot2)
 library(soilpalettes)
 library(soilDB)
--- code/ER-23-SOMMOSdistributionplots.R
+++ code/ER-23-SOMMOSdistributionplots.R
@@ -4,31 +4,100 @@
 #load data-------------------------------------
 sommos_csv = read.csv("processed/horizon_processed4.csv")
 
+# ggplot set up-----------------------------------
+theme_er <- function() {  # this for all the elements common across plots
+  theme_bw() %+replace%
+    theme(legend.position = "top",
+          legend.key=element_blank(),
+          legend.title = element_blank(),
+          legend.text = element_text(size = 12),
+          legend.key.size = unit(1.5, 'lines'),
+          panel.border = element_rect(color="black",size=2, fill = NA),
+          plot.title = element_text(hjust = 0.5, size = 14),
+          plot.subtitle = element_text(hjust = 0.5, size = 12, lineheight = 1.5),
+          axis.text = element_text(size = 12, color = "black"),
+          axis.title = element_text(size = 12, face = "bold", color = "black"),
+          # formatting for facets
+          panel.background = element_blank(),
+          strip.background = element_rect(colour="white", fill="white"), #facet formatting
+          panel.spacing.x = unit(1.5, "lines"), #facet spacing for x axis
+          panel.spacing.y = unit(1.5, "lines"), #facet spacing for x axis
+          strip.text.x = element_text(size=12, face="bold"), #facet labels
+          strip.text.y = element_text(size=12, face="bold", angle = 270) #facet labels
+    )
+}
 
-
-#select data-----------------------------------
-barrow = sommos_csv$site=="BARR"
-healy = sommos_csv$site=="HEAL"
-toolik = sommos_csv$site=="TOOL"
-bona = sommos_csv$site=="BONA"
-data = as.data.frame(barrow)
-data = as.data.frame(healy)
-data = as.data.frame(toolik)
-data = as.data.frame(bona)
+theme_jack <- function (base_size = 12, base_family = "") {
+  theme_bw(base_size = base_size, base_family = base_family) %+replace% 
+    theme(
+      legend.position = "top",
+      legend.key=element_blank(),
+      legend.title = element_blank(),
+      legend.text = element_text(size = 12),
+      legend.key.size = unit(1.5, 'lines'),
+      panel.border = element_rect(color="gray",size=2, fill = NA),
+      axis.text = element_text(colour = "black"),
+      axis.title.x = element_text(colour = "black"), #size=rel(3)),
+      axis.title.y = element_text(colour = "black", angle=90),
+      panel.background = element_rect(fill="black"),
+      panel.grid.minor = element_line(color= "dark gray"),
+      panel.grid.major = element_line(colour = "dark gray"),
+      plot.background = element_rect(fill="white"))
+}
+      
+# select data-----------------------------------
+# barrow = sommos_csv$site=="BARR"
+# healy = sommos_csv$site=="HEAL"
+# toolik = sommos_csv$site=="TOOL"
+# bona = sommos_csv$site=="BONA"
+# data = as.data.frame(barrow)
+# data = as.data.frame(healy)
+# data = as.data.frame(toolik)
+# data = as.data.frame(bona)
 
 #load library--------------------------------
-library(ggplot2)
+library(tidyverse)
+library(PNWColors)
 
-#plot Fe distributions-------------------------
+#aov---------------------------------------
+
+sommos_aov1 = aov(data = sommos_csv, LIG.ugg ~ site)
+summary(sommos_aov1)
 
 
-p <- ggplot() +
+
+
+
+#ggplots------------------------------------------------------------
+
+sommos_csv %>% 
+  ggplot() +
+  geom_point(data = sommos_csv, aes(y=HF.g100g, x=OCC.g100g, color=site)) +
+  theme_er()
+  #scale_y_continuous(trans = "reverse", breaks = (sommos_csv$top_depth.cm)) 
+  #facet_grid(. ~ site)
+
+sommos_csv %>% 
+  ggplot() +
+  geom_point(data = sommos_csv, aes(y=OCC.g100g, x=LIG.ugg, color=site, size=10, shape=horizon_type)) +
+  theme_jack() +
+  scale_color_manual(values = rev(PNWColors::pnw_palette("Bay")))
   
-  geom_line(data = sommos_csv, aes(y=top_depth.cm, x=OC.g100g, color=site)) +
   
-  scale_y_continuous(trans = "reverse", breaks = (sommos_csv$top_depth.cm))
+#scale_y_continuous(trans = "reverse", breaks = (sommos_csv$top_depth.cm)) 
+#facet_grid(. ~ site)
 
-p + facet_grid(. ~ site)
+sommos_csv %>% 
+  ggplot() +
+  geom_point(data = sommos_csv, aes(y=FLF.g100g, x=HF.g100g, color=site, size=OCC.g100g)) +
+  #scale_y_continuous(trans = "reverse", breaks = (sommos_csv$top_depth.cm)) +
+  theme_er2() +
+  strip.background = element_rect(colour="black", fill="black") #facet formatting
+
+  #facet_grid(. ~ site)
+
+###########
+
 
 
 p <- ggplot() +
