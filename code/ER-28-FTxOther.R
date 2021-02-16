e0b440e444423eca734d663f90e73177ad997ca6
Erin-Rooney <61806923+Erin-Rooney@users.noreply.github.com>
2021-02-16 15:48:54 -0800

    Update ER-28-FTxOther.R

--- code/ER-28-FTxOther.R
+++ code/ER-28-FTxOther.R
@@ -15,6 +15,59 @@ library(soilpalettes)
 library(PNWColors)
 library(nord)
 
+
+# 3. SET GGPLOT THEME -----------------------------------------------------
+
+theme_kp <- function() {  # this for all the elements common across plots
+  theme_bw() %+replace%
+    theme(legend.position = "right",
+          legend.key=element_blank(),
+          legend.title = element_blank(),
+          legend.text = element_text(size = 12),
+          legend.key.size = unit(1.5, 'lines'),
+          panel.border = element_rect(color="white",size=1.5, fill = NA),
+          
+          plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
+          plot.subtitle = element_text(hjust = 0.5),
+          axis.text = element_text(size = 10, color = "black"),
+          axis.title = element_text(size = 12, face = "bold", color = "black"),
+          
+          # formatting for facets
+          panel.background = element_blank(),
+          strip.background = element_rect(colour="white", fill="white"), #facet formatting
+          panel.spacing.x = unit(1.5, "lines"), #facet spacing for x axis
+          panel.spacing.y = unit(1.5, "lines"), #facet spacing for x axis
+          strip.text.x = element_text(size=12, face="bold"), #facet labels
+          strip.text.y = element_text(size=12, face="bold", angle = 270) #facet labels
+    )
+}
+
+theme_kpnone <- function() {  # this for all the elements common across plots
+  theme_bw() %+replace%
+    theme(legend.position = "none",
+          legend.key=element_blank(),
+          legend.title = element_blank(),
+          legend.text = element_text(size = 12),
+          legend.key.size = unit(1.5, 'lines'),
+          panel.border = element_rect(color="white",size=1.5, fill = NA),
+          
+          plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
+          plot.subtitle = element_text(hjust = 0.5),
+          axis.text = element_text(size = 10, color = "black"),
+          axis.title = element_text(size = 12, face = "bold", color = "black"),
+          
+          # formatting for facets
+          panel.background = element_blank(),
+          strip.background = element_rect(colour="white", fill="white"), #facet formatting
+          panel.spacing.x = unit(1.5, "lines"), #facet spacing for x axis
+          panel.spacing.y = unit(1.5, "lines"), #facet spacing for x axis
+          strip.text.x = element_text(size=12, face="bold"), #facet labels
+          strip.text.y = element_text(size=12, face="bold", angle = 270) #facet labels
+    )
+}
+
+# Load data------------------------------------------
+
 # ghg_data = read.csv("processed/ghg_depth.csv")
 ftc_data = read.csv("processed/FTC_quant_inprocess.csv")
 ftc_full = read.csv("processed/final_dat2.csv")
@@ -23,7 +76,9 @@ neon_heal_csv = read.csv("processed/neon_heal_biogeochem.csv")
 neon_tool_csv = read.csv("processed/neon_tool_biogeochem.csv")
 neon_bona_csv = read.csv("processed/neon_bona_biogeochem.csv")
 site_data = read.csv("processed/SOMMOS_Site_11-25-19.csv")
-climate_data = read.csv("processed/SOMMOS_SoilCoreDB - Climate_1961_1990.csv")
+# climate_data = read.csv("processed/SOMMOS_SoilCoreDB - Climate_1961_1990.csv")
+climate_data = read.csv("processed/climate_xseason_sommos.csv")
+
 
 #
 # 2. process files -----------------------------------------------------------
@@ -32,22 +87,29 @@ climate_data = read.csv("processed/SOMMOS_SoilCoreDB - Climate_1961_1990.csv")
 precip = climate_data %>% 
  filter(site %in% c("BONA", "BARR", "HEAL", "TOOL")) %>% 
   mutate(site = factor(site, levels = c("BARR", "TOOL", "BONA", "HEAL"))) %>%
-  select(site, 'PPT01', 'PPT02', 'PPT03', 'PPT04', 'PPT05', 'PPT06', 'PPT07', 'PPT08', 'PPT09') %>% 
+  select(site, 'PPT_wt', 'PPT_sp', 'PPT_sm', 'PPT_at') %>% 
+  # select(site, 'PPT01', 'PPT02', 'PPT03', 'PPT04', 'PPT05', 'PPT06', 'PPT07', 'PPT08', 'PPT09') %>% 
   tidyr::pivot_longer(
     cols = starts_with("PPT"),
-    names_to = "precip_type",
-    values_to = "precip_cm")
+    names_to = "season",
+    values_to = "precip_cm") %>% 
+  mutate(season = factor(season, levels = c('PPT_wt', 'PPT_sp', 'PPT_sm', 'PPT_at')),
+                                   season = recode(season, "PPT_wt" = "Winter",
+                                                   "PPT_sp" = "Spring",
+                                                   "PPT_sm" = "Summer",
+                                                   "PPT_at" = "Fall")) 
 
 precip %>% 
   ggplot(aes(x=site, y = precip_cm, fill = site))+
   geom_point()+
   geom_boxplot(alpha = 0.5)+
   theme_kp()+
+  facet_wrap(.~season)+
   scale_fill_nord("afternoon_prarie", 4)+
   labs(y = "precipitation, cm")
 
 precip %>% 
-  ggplot(aes(x=precip_type, y = precip_cm, color = site, group = site))+
+  ggplot(aes(x=season, y = precip_cm, color = site, group = site))+
   geom_point(size = 5)+
   geom_line(size = 2)+
   #geom_boxplot(alpha = 0.5)+
@@ -55,46 +117,62 @@ precip %>%
   scale_color_nord("afternoon_prarie", 4)+
   labs(y = "precipitation, cm")
 
-temp %>% 
-  mutate(Temp_type = factor(Temp_type, levels = c("max", "ave", "min")),
-         Temp_type = recode(Temp_type, "max" = "Maximum Temperature",
-                            "ave" = "Average Temperature",
-                            "min" = "Minimum Temperature")) %>% 
-  ggplot(aes(x=site, y = Temp_C, fill = site))+
-  geom_point()+
-  geom_boxplot(alpha = 0.5)+
-  theme_kp()+
-  scale_fill_nord("afternoon_prarie", 4)+
-  labs(y = "Temperature, C")+
-  facet_grid(.~Temp_type)
 
 temp = climate_data %>% 
   filter(site %in% c("BONA", "BARR", "HEAL", "TOOL")) %>% 
   mutate(site = factor(site, levels = c("BARR", "TOOL", "BONA", "HEAL"))) %>%
-  select(site, 'Tmax01', 'Tmax02', 'Tmax03', 'Tmax04', 'Tmax05', 'Tmax06', 'Tmax07', 'Tmax08', 'Tmax09', 'Tmax10', 'Tmax11', 'Tmax12', 
-         'Tmin01', 'Tmin02', 'Tmin03', 'Tmin04', 'Tmin05', 'Tmin06', 'Tmin07', 'Tmin08', 'Tmin09', 'Tmin10', 'Tmin11', 'Tmin12',
-         'Tave01', 'Tave02', 'Tave03', 'Tave04', 'Tave05', 'Tave06', 'Tave07', 'Tave08', 'Tave09', 'Tave10', 'Tave11', 'Tave12') %>% 
+  select(site, 'Tave_wt', 'Tave_sp', 'Tave_sm', 'Tave_at') %>% 
+  # select(site, 'PPT01', 'PPT02', 'PPT03', 'PPT04', 'PPT05', 'PPT06', 'PPT07', 'PPT08', 'PPT09') %>% 
   tidyr::pivot_longer(
-    cols = starts_with("T"),
-    names_to = "Temp_ID",
-    values_to = "Temp_C") %>% 
-  mutate(Temp_type = case_when(grepl("Tmax", Temp_ID)~"max",
-                               grepl("Tave", Temp_ID)~"ave",
-                               grepl("Tmin", Temp_ID)~"min")
-         
-         )
+    cols = starts_with("Tave"),
+    names_to = "season",
+    values_to = "temp_C") %>% 
+  mutate(season = factor(season, levels = c('Tave_wt', 'Tave_sp', 'Tave_sm', 'Tave_at')),
+         season = recode(season, "Tave_wt" = "Winter",
+                         "Tave_sp" = "Spring",
+                         "Tave_sm" = "Summer",
+                         "Tave_at" = "Fall")) 
 
+# temp = climate_data %>% 
+#   filter(site %in% c("BONA", "BARR", "HEAL", "TOOL")) %>% 
+#   mutate(site = factor(site, levels = c("BARR", "TOOL", "BONA", "HEAL"))) %>%
+#   select(site, 'Tmax01', 'Tmax02', 'Tmax03', 'Tmax04', 'Tmax05', 'Tmax06', 'Tmax07', 'Tmax08', 'Tmax09', 'Tmax10', 'Tmax11', 'Tmax12', 
+#          'Tmin01', 'Tmin02', 'Tmin03', 'Tmin04', 'Tmin05', 'Tmin06', 'Tmin07', 'Tmin08', 'Tmin09', 'Tmin10', 'Tmin11', 'Tmin12',
+#          'Tave01', 'Tave02', 'Tave03', 'Tave04', 'Tave05', 'Tave06', 'Tave07', 'Tave08', 'Tave09', 'Tave10', 'Tave11', 'Tave12') %>% 
+#   tidyr::pivot_longer(
+#     cols = starts_with("T"),
+#     names_to = "Temp_ID",
+#     values_to = "Temp_C") %>% 
+#   mutate(Temp_type = case_when(grepl("Tmax", Temp_ID)~"max",
+#                                grepl("Tave", Temp_ID)~"ave",
+#                                grepl("Tmin", Temp_ID)~"min")
+         # 
+         # )
+
+
+# temp %>% 
+#   mutate(Temp_type = factor(Temp_type, levels = c("max", "ave", "min")),
+#          Temp_type = recode(Temp_type, "max" = "Maximum Temperature",
+#                             "ave" = "Average Temperature",
+#                             "min" = "Minimum Temperature")) %>% 
+#   ggplot(aes(x=site, y = Temp_C, fill = site))+
+#   geom_point()+
+#   geom_boxplot(alpha = 0.5)+
+#   theme_kp()+
+#   scale_fill_nord("afternoon_prarie", 4)+
+#   labs(y = "Temperature, C")+
+#   facet_grid(.~Temp_type)
 
 temp %>% 
-  filter(Temp_type %in% "ave") %>% 
-  ggplot(aes(x=Temp_ID, y = Temp_C, color = site, group = site))+
+  #filter(Temp_type %in% "ave") %>% 
+  ggplot(aes(x=season, y = temp_C, color = site, group = site))+
   geom_point(size = 5)+
   geom_line(size =2)+
   #geom_boxplot(alpha = 0.5)+
   theme_kp()+
   scale_color_nord("afternoon_prarie", 4)+
-  labs(y = "Temperature, C")+
-  facet_grid(.~Temp_type)
+  labs(y = "Temperature, C")
+  #facet_grid(.~Temp_type)
 
 ## 2.1 calculate mean ftc
 
@@ -240,55 +318,6 @@ om_ftc %>%
 
 #ggplot geom_rect--------------------------------------------------
 
-# 3. SET GGPLOT THEME -----------------------------------------------------
-
-theme_kp <- function() {  # this for all the elements common across plots
-  theme_bw() %+replace%
-    theme(legend.position = "right",
-          legend.key=element_blank(),
-          legend.title = element_blank(),
-          legend.text = element_text(size = 12),
-          legend.key.size = unit(1.5, 'lines'),
-          panel.border = element_rect(color="white",size=1.5, fill = NA),
-          
-          plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
-          plot.subtitle = element_text(hjust = 0.5),
-          axis.text = element_text(size = 10, color = "black"),
-          axis.title = element_text(size = 12, face = "bold", color = "black"),
-          
-          # formatting for facets
-          panel.background = element_blank(),
-          strip.background = element_rect(colour="white", fill="white"), #facet formatting
-          panel.spacing.x = unit(1.5, "lines"), #facet spacing for x axis
-          panel.spacing.y = unit(1.5, "lines"), #facet spacing for x axis
-          strip.text.x = element_text(size=12, face="bold"), #facet labels
-          strip.text.y = element_text(size=12, face="bold", angle = 270) #facet labels
-    )
-}
-
-theme_kpnone <- function() {  # this for all the elements common across plots
-  theme_bw() %+replace%
-    theme(legend.position = "none",
-          legend.key=element_blank(),
-          legend.title = element_blank(),
-          legend.text = element_text(size = 12),
-          legend.key.size = unit(1.5, 'lines'),
-          panel.border = element_rect(color="white",size=1.5, fill = NA),
-          
-          plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
-          plot.subtitle = element_text(hjust = 0.5),
-          axis.text = element_text(size = 10, color = "black"),
-          axis.title = element_text(size = 12, face = "bold", color = "black"),
-          
-          # formatting for facets
-          panel.background = element_blank(),
-          strip.background = element_rect(colour="white", fill="white"), #facet formatting
-          panel.spacing.x = unit(1.5, "lines"), #facet spacing for x axis
-          panel.spacing.y = unit(1.5, "lines"), #facet spacing for x axis
-          strip.text.x = element_text(size=12, face="bold"), #facet labels
-          strip.text.y = element_text(size=12, face="bold", angle = 270) #facet labels
-    )
-}
 
 
 ftc_avg_depth = ftc_avg %>% 
