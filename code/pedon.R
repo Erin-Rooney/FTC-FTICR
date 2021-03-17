b8d4ea6951308c6e7eb5fafb018f2962da845c98
Erin-Rooney <61806923+Erin-Rooney@users.noreply.github.com>
2021-03-17 13:36:21 -0700

    Create pedon.R

---
+++ code/pedon.R
@@ -0,0 +1,43 @@
+library(aqp)
+library(plyr)
+library(RColorBrewer)
+library(latticeExtra)
+library(plyr)
+library(reshape)
+library(tidyverse)
+
+
+#load csv
+pedon <- read.csv("processed/ped_profiles_munsell_dry.csv", stringsAsFactors=FALSE)
+
+pedon %>% 
+  
+
+#look at first several lines of imported file
+head(pedon)
+
+#double-check structure of the data
+str(pedon)
+
+#gives the class of the object
+class(pedon)
+
+#create new column for hex color and convert munsell color to hex format
+pedon$soilcolor <- munsell2rgb(pedon$hue, pedon$value, pedon$chroma)
+
+
+#double-check new "soil color" column
+print(pedon)
+
+#convert to apq object "soil profile collection"
+depths(pedon) <- site ~ top + bottom
+
+
+#check the new class
+str(pedon)
+class(pedon)
+summary(pedon)
+
+#plot
+plot(pedon, name = 'horizon', color = 'soilcolor') 
+
