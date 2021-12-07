526f8b96e244b0af79f2ec9356a8c53cc2415159
Erin-Rooney <61806923+Erin-Rooney@users.noreply.github.com>
2021-12-07 15:58:33 -0800

    manuscript van krev method figure changes and soil texture analysis

---
+++ code/method-7-soilpropertiesSHL.R
@@ -0,0 +1,59 @@
+#ECRooney
+#2021 12 6
+#soil properties and texture
+#fticr manuscript
+
+#load libraries-------------------------------
+source("code/0-method-packages.R")
+
+
+#load data-------------------------------------
+properties = read.csv("processed/2021_12_6_shl_soilproperties.csv")
+texture = read.csv("processed/2021_12_6_shl_soiltexture.csv")
+
+#dataprocessing table---------------------
+
+texture_long = 
+  texture %>% 
+  pivot_longer(-c(Customer.ID, Site, Trtmt, Rep, depth_top_cm, depth_bottom_cm, 
+                  Morph, Material, labID, grav_moist), names_to = "particle_size", 
+                  values_to = "data") %>% 
+  mutate (particle_size = factor(particle_size, levels = c("Clay_.2mm", "Silt_2to20mm",
+                                                           "Silt_20to50mm", "Sand_50to100mm",
+                                                           "Sand_100to250mm", "Sand_250to500mm",
+                                                           "Sand_500to1000mm", "Sand_1000to2000mm"))) %>%
+  na.omit() %>% 
+  group_by(Site, Material, particle_size) %>% 
+  dplyr::summarise(mean = round(mean(data), 2),
+                   se = round(sd(data)/sqrt(n()),2)) %>% 
+  mutate(summary = paste(mean, "\u00b1", se)) %>% 
+  dplyr::select(-mean, -se)
+
+#clean
+
+texture_long %>% knitr::kable() # prints a somewhat clean table in the console
+
+#export
+
+write.csv(texture_long, "output/texture_long.csv", row.names = FALSE)
+
+  
+#ggplot----------
+
+texture_long %>% 
+  mutate(Site = recode(Site, "TOOL" = "Toolik",
+                       "HEAL" = "Healy")) %>% 
+  ggplot(aes(x = Material, y = data, fill = Site))+
+  labs(x = " ",
+       y = "%")+
+  #geom_bar(aes(fill = particle_size), stat = "identity")+
+  geom_boxplot()+
+  scale_fill_manual(values = (pnw_palette("Shuksan2",2)))+
+  facet_wrap(. ~ particle_size)+
+  #geom_text(data = label, aes(x = Trtmt, y = y, label = label), size = 8, color = "white")+
+  theme_er()+
+  theme(legend.position = "bottom", panel.border = element_rect(color="white",size=0.5, fill = NA), 
+        axis.text.x.bottom = element_text (vjust = 0.5, hjust=1, angle = 90)
+        
+  )+
+  NULL
